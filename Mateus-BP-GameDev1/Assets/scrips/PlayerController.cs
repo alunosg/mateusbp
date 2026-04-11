70fece31e4b340e4b2e182bb2951c7bc92c34102
JOPE3778 <jp7maciel@gmail.com>
2026-04-11 12:14:57 -0300

    Attack

--- Mateus-BP-GameDev1/Assets/scrips/PlayerController.cs
+++ Mateus-BP-GameDev1/Assets/scrips/PlayerController.cs
@@ -9,7 +9,19 @@ public class Playercontroller : MonoBehaviour
     public float jumpForce = 10;
     public LayerMask floorLayer;
     public Animator anim;
-    public bool grounded =false;
+
+    public float attackDuration = 1.0f;
+
+    public float airAttackDuration = 1.0f;
+
+    public GameObject attackFX, attackHitFX, hitFX, deathFX;
+
+    public Transform attackPoint; 
+    
+    private bool grounded =false;
+
+    private bool locked  =false;
+
 
     private Vector2 moveInput;
 
@@ -18,17 +30,33 @@ public class Playercontroller : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
         anim.SetBool("IsMoving", moveInput.x != 0);
+        transform.localScale = new(
+            moveInput.x > 0 ? 1 : moveInput.x < 0 ? -1 : transform.localScale.x, 1);
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.started && grounded)
+        if(!locked && context.started && grounded)
         {
             anim.SetTrigger("jump");
             rig.linearVelocity = new(rig.linearVelocity.x, jumpForce);
         }
     }
 
+    public void AttackInput(InputAction.CallbackContext context)
+    {
+        if (!locked && context.started)
+        {
+            
+            anim.SetTrigger("Attack");
+            locked = true;
+            if (attackFX) Instantiate(attackFX, attackPoint.position, attackPoint.rotation);
+            Invoke(nameof(Unlock), grounded ? attackDuration : airAttackDuration);
+        }
+    }
+
+    private void Unlock() => locked =false;
+       
     public void GroundCheck()
     {
         Vector2 leftPoint = new(col.bounds.min.x, col.bounds.max.y);
@@ -50,7 +78,9 @@ public class Playercontroller : MonoBehaviour
     public void FixedUpdate()
     {
         GroundCheck();
-        rig.linearVelocity = new Vector2(moveInput.x * speed, rig.linearVelocity.y);
+        rig.linearVelocity = new Vector2(
+            locked && grounded ? 0 : speed * moveInput.x, 
+            rig.linearVelocity.y);
     }
 
 }
